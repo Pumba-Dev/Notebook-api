f3848c011ce2aa2d05766fa9cae783855e230d99
PumbaSD <dudu32769144@gmail.com>
2021-11-26 20:37:49 -0300

    Studying render method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,9 @@
 class Contact < ApplicationRecord
+    def author
+        "PumbaSD"
+    end
+
+    def as_json(options={})
+        super(methods: :author, root: true, only: [:name, :email, :birthdate])
+    end
 end
