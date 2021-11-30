0c9befb88edfef645dedf878eb0182ab096344a4
PumbaSD <dudu32769144@gmail.com>
2021-11-30 13:21:46 -0300

    Mime Type Fixed

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,13 +2,10 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
   
   belongs_to :kind do
-    link(:kind) { kind_url(object.kind.id) }
+    link(:related) { kind_url(object.kind.id) }
   end
   has_many :phones
   has_one :adress
-
-  link(:self) { contact_url(object.id) }
-  
 
   def attributes(*args)
     hashObject = super(*args)
