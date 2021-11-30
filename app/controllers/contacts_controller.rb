de6a7615cd6f576e58b9911f4a92c1f9965ce469
PumbaSD <dudu32769144@gmail.com>
2021-11-30 12:59:36 -0300

    RestFul Relationship Links

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,12 +5,12 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
 
-    render json: @contacts, include: [:kind, :phones, :adress]
+    render json: @contacts
   end
 
   # GET /contacts/1
   def show
-    render json: @contact, include: [:kind, :phones, :adress]
+    render json: @contact
   end
 
   # POST /contacts
@@ -18,7 +18,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
 
     if @contact.save
-      render json: @contact, include: [:kind, :phones, :adress], status: :created, location: @contact
+      render json: @contact, status: :created, location: @contact
     else
       render json: @contact.errors, status: :unprocessable_entity
     end
@@ -27,7 +27,7 @@ class ContactsController < ApplicationController
   # PATCH/PUT /contacts/1
   def update
     if @contact.update(contact_params)
-      render json: @contact, include: [:kind, :phones, :adress]
+      render json: @contact
     else
       render json: @contact.errors, status: :unprocessable_entity
     end
